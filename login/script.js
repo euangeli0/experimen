f1a66eea77430cd4fdbf02d933038cdf30b17b8c
euangelio <161925380+euangeli0@users.noreply.github.com>
2025-12-16 05:54:40 -1200

    Add files via upload

---
+++ login/script.js
@@ -0,0 +1,24 @@
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const user = document.getElementById("username").value;
+  const pass = document.getElementById("password").value;
+  const msg = document.getElementById("message");
+
+  // akun default
+  const validUser = "admin";
+  const validPass = "admin";
+
+  if (user === validUser && pass === validPass) {
+    msg.style.color = "lightgreen";
+    msg.textContent = "Login berhasil...";
+
+    setTimeout(() => {
+      window.location.href = "/halaman/halaman.html";
+    }, 1500);
+
+  } else {
+    msg.style.color = "red";
+    msg.textContent = "Username atau password salah!";
+  }
+});
